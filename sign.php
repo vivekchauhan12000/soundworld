a459bebc1157a6d833dab73e3679f5140c2c203e
vivekchauhan12000 <49834398+vivekchauhan12000@users.noreply.github.com>
2020-09-05 15:54:12 +0530

    updated

--- sign.php
+++ sign.php
@@ -6,8 +6,7 @@ $con = mysqli_connect('localhost','root','');
 
 mysqli_select_db($con,'userregistration');
 
-$name = $_POST['user'];
-$pass = $_POST['password'];
+
 
 
 $s = "select * from usertable where name ='$name' && password = '$pass'";
