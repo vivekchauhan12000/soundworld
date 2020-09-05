a03099d860d192c4441ce3c2c8a58ab9ff2acf16
vivekchauhan12000 <49834398+vivekchauhan12000@users.noreply.github.com>
2020-09-05 15:53:00 +0530

    commit_2

--- main.php
+++ main.php
@@ -6,9 +6,7 @@ $con = mysqli_connect('localhost','root','');
 
 mysqli_select_db($con,'userregistration');
 
-$name = $_POST['user'];
-$pass = $_POST['password'];
-$email = $_POST['email'];
+
 
 $s = "select * from usertable where name ='$name'";
 
